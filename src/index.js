c589f758e22865fa084fe856645c6d3d78d76c8c
Elena Kichinskaia <nitress@gmail.com>
2021-02-16 20:48:04 +0200

    creates markup using handlebars template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+
 // Тема
 // Добавь функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
 
@@ -15,6 +16,7 @@ const Theme = {
 
 const bodyRef = document.querySelector('body');
 const inputRef = document.querySelector('#theme-switch-toggle');
+const galleryRef = document.querySelector('.js-menu')
 
 const savedDarkTheme = () => {
     if (localStorage.getItem('Theme') === Theme.DARK) {
@@ -25,7 +27,7 @@ const savedDarkTheme = () => {
 
 savedDarkTheme();
     
-function themeSet() {
+function handleThemeSet(event) {
     if (localStorage.getItem('Theme') === Theme.DARK) {
         localStorage.setItem('Theme', Theme.LIGHT);
         bodyRef.classList.remove(`${Theme.DARK}`);
@@ -38,13 +40,66 @@ function themeSet() {
         };
 };
 
-inputRef.addEventListener('input', () => {
-    themeSet();
-});
+inputRef.addEventListener('change', handleThemeSet);
+
+// Шаблонизация
+// Используя шаблонизатор Handlebars создай шаблон одного элемента меню. После чего, используя шаблон, 
+// создай разметку всего меню по данным из menu.json и добавь в DOM в ul.js - menu.
+
+// Для иконок используется библиотека Material Icons, линк на веб-фонт уже есть в исходном HTML.
+
+// Ниже указана разметка элемента меню которая должна получаться по шаблону (данные будут разные для каждого объекта).
+
+/* <li class="menu__item">
+  <article class="card">
+    <img
+      src="https://s1.eda.ru/StaticContent/Photos/140812180013/140820212258/p_O.jpg"
+      alt="Картофель, запеченный в мундире"
+      class="card__image"
+    />
+    <div class="card__content">
+      <h2 class="card__name">Картофель, запеченный в мундире</h2>
+      <p class="card__price">
+        <i class="material-icons"> monetization_on </i>
+        100 кредитов
+      </p>
+
+      <p class="card__descr">
+        Ароматный, сытный, шипящий домашний картофель, фаршированный
+        сметанно-беконной начинкой, — это очень простой и очень эффектный способ
+        накормить большое количество человек, практически не потратив на готовку
+        ни сил, ни времени. Обычную картошку при желании тут можно заменить на
+        сладкий батат, а в начинку добавить, к примеру, кукурузу или сладкий
+        красный перец.
+      </p>
+
+      <ul class="tag-list">
+        <li class="tag-list__item">Картофель</li>
+        <li class="tag-list__item">Чеснок</li>
+        <li class="tag-list__item">Сметана</li>
+        <li class="tag-list__item">Бекон</li>
+        <li class="tag-list__item">Твердый сыр</li>
+        <li class="tag-list__item">Зеленый лук</li>
+      </ul>
+    </div>
+
+    <button class="card__button button">
+      <i class="material-icons button__icon"> shopping_cart </i>
+      В корзину
+    </button>
+  </article>
+</li> */
 
 
+import itemsTemplate from '../templates/gallery-items.hbs'
+console.log(itemsTemplate);
+import items from './menu.json'
+console.log(items);
+console.log(items);
 
 
+const markup = itemsTemplate(items);
+console.log(markup);
 
-
+galleryRef.insertAdjacentHTML('beforeend', markup)
 
